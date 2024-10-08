56f177f559ade55e422750ba1db00a03141e7363
victoria-rivaspa13 <162721841+victoria-rivaspa13@users.noreply.github.com>
2024-10-07 22:34:34 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,33 @@
-def index_of_by_index(word, list, index):
+def index_of_by_index(word, lst, index):
+    for i in range(index, len(lst)):
+        if lst[i] == word:
+            return i
     return -1
-
 
 def index_of_empty(list):
+    for i, item in enumerate(list):
+        if item == "":
+            return i
     return -1
 
-
 def index_of(word, list):
+    for i, item in enumerate(list):
+        if item == word:
+            return i
     return -1
 
 
 def put(word, list):
+    for i, item in enumerate(list):
+        if item == "":
+            list[i] = word
+            return i
     return -1
-
 
 def remove(word, list):
-    return -1
+    count = 0
+    for i, item in enumerate(list):
+        if item == word:
+            list[i] = ""
+            count += 1
+    return count
